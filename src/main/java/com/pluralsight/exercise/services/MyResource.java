4fe7d85be62b8e2e446071a9ea46a456f7d418b8
zack.beck <zack.beck@stgconsulting.com>
2016-02-03 16:28:01 -0700

    Second commit to test git setup on a new branch

--- src/main/java/com/pluralsight/exercise/services/MyResource.java
+++ src/main/java/com/pluralsight/exercise/services/MyResource.java
@@ -20,6 +20,6 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "GET WORKS!";
+        return "GET is Working!!!!! Look how awesome you are!";
     }
 }
