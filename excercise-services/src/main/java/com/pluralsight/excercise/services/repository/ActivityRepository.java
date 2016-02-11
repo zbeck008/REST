22b5b669e32a46b5ec65717984de2ff0bf55f237
Zack Beck <zbeck008@gmail.com>
2016-02-11 15:02:12 -0700

    FIXED! Learned to fix errors and get to a working url/api endpoint

--- excercise-services/src/main/java/com/pluralsight/excercise/services/repository/ActivityRepository.java
+++ excercise-services/src/main/java/com/pluralsight/excercise/services/repository/ActivityRepository.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.pluralsight.excercise.services.repository;
 
+import com.pluralsight.excercise.services.model.Activity;
 import java.util.List;
 
 /**
